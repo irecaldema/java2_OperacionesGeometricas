b225c7478a18f9b6210c5926f0861b9d0e3d71e4
irecaldema14dw@ikzubirimanteo.com <zubiri@Ubuntu-12-04.(none)>
2014-09-30 08:39:40 +0200

    version 1

---
+++ src/OperacionesGeometricas.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+import geometria.*;
+
+public class OperacionesGeometricas 
+{
+
+public static void main (String args[]) 
+	{
+ string opcion;
+ double radio, base, altura;
+
+ Scanner sc = new Scanner(System.in);
+ Circulo circu = new Circulo();
+ Rectangulo recta = new Rectangulo();
+
+ System.out.print("\nElija la figura que quiere analizar: (Rectangulo/Circulo) ");
+
+ opcion = sc.next();
+
+ if(opcion.equalsIgnoreCase("circulo"){
+
+  System.out.print("\ningresa el radio del circulo a analizar: ");
+ 
+  Scanner sc = new Scanner(System.in);
+  System.out.print("\ningresa el radio del circulo a analizar: ");
+  radio = sc.nextDouble();
+  operaciones.setRadio(radio);
+
+  System.out.println("\nEl area del circulo es: " + circu.Area());
+  System.out.println("\nLa circunferencia del circulo es: " + circu.Circu());
+
+ }
+
+ if(opcion.equalsIgnoreCase("rectangulo") {
+
+  System.out.print("\ningresa la base del rectangulo a analizar: ");
+  base = sc.nextDouble();
+  operaciones.setBase(base);
+
+  System.out.print("\ningresa la base del rectangulo a analizar: ");
+  base = sc.nextDouble();
+  operaciones.setAltura(altura);
+
+  System.out.println("\nEl area del rectangulo es: " + recta.Area());
+  System.out.println("\nEl Perimetro del rectangulo es: " + recta.Peri());
+
+ }
+
+ else{
+
+  System.out.println("\nVa a ser que no has escrito bien...");
+  System.out.println("\nPrueba otra vez...");
+
+ }
+
+ System.out.println("\n\nEGUN ONA IZAN!!\n");
+	}
+}
