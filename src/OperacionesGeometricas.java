862d72255ade7a0de367d998011519a475fd1219
irecaldema14dw@ikzubirimanteo.com <zubiri@Ubuntu-12-04.(none)>
2014-10-09 08:38:12 +0200

    version 5 while+char

--- src/OperacionesGeometricas.java
+++ src/OperacionesGeometricas.java
@@ -1,23 +1,20 @@
 import java.util.Scanner;
 //CLASSPATH=:/home/zubiri/ProyectosJava; export CLASSPATH
 import com.zubiri.geometria.*;
-
 public class OperacionesGeometricas 
 {
-
 public static void main (String args[]) 
 	{
-
- String opcion;
+ char opcion;
  double radio, base, altura;
-
  Scanner sc = new Scanner(System.in);
+ 
+ System.out.print("\nElija la figura que quiere analizar: Rectangulo (R)  Circulo (C) Terminar (T)  "); 
+ opcion = sc.next().charAt(0);
 
- System.out.print("\nElija la figura que quiere analizar: (Rectangulo/Circulo) ");
-
- opcion = sc.next();
-
- if(opcion.equalsIgnoreCase("circulo"))
+ while (opcion!='T'&&opcion!='t')
+			{
+ if(opcion=='C'||opcion=='c')
  {
 
   System.out.print("\ningresa el radio del circulo a analizar: ");
@@ -25,13 +22,13 @@ public static void main (String args[])
  
   Circulo circu = new Circulo(radio);
 
-  System.out.println("\nEl area del circulo es: " + circu.Area());
-  System.out.println("\nLa circunferencia del circulo es: " + circu.Circu());
+  System.out.println("\nEl area del circulo es: " + circu.area());
+  System.out.println("\nLa circunferencia del circulo es: " + circu.circu());
 
  }
  else 
  {
-  if(opcion.equalsIgnoreCase("rectangulo")) 
+  if(opcion=='R'||opcion=='r') 
   {
  
    System.out.print("\ningresa la base del rectangulo a analizar: ");
@@ -42,19 +39,20 @@ public static void main (String args[])
    
    Rectangulo recta = new Rectangulo(base,altura);
 
-   System.out.println("\nEl area del rectangulo es: " + recta.Area());
-   System.out.println("\nEl Perimetro del rectangulo es: " + recta.Peri());
+   System.out.println("\nEl area del rectangulo es: " + recta.area());
+   System.out.println("\nEl Perimetro del rectangulo es: " + recta.peri());
 
   }
   else
   {
-
    System.out.println("\nVa a ser que no has escrito bien...");
    System.out.println("\nPrueba otra vez...");
-
   }
  }
  
+ System.out.print("\nElija la figura que quiere analizar: Rectangulo (R)  Circulo (C) Terminar (T)"); 
+ opcion = sc.next().charAt(0);
+			}
  System.out.println("\n\nEGUN ONA IZAN!!\n");
 	}
 }
